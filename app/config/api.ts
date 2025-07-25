6c1129aedb47c7a09ec2e892ee384c2a657fd20e
Soss <l56eerl@gmail.com>
2025-07-25 18:10:50 +0300

    F

--- app/config/api.ts
+++ app/config/api.ts
@@ -3,5 +3,5 @@ export const API_URL = import.meta.env.VITE_API_URL;
 
 
 export const getApiUrl = (path: string) => {
-  return `${API_URL}${path.startsWith('/') ? path : `/${path}`}`;
+  return API_URL;
 }; 
