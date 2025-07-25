3f00661d4ada2dbaa6a5731520914934e1a20770
Soss <l56eerl@gmail.com>
2025-07-25 17:30:32 +0300

    f

--- app/config/api.ts
+++ app/config/api.ts
@@ -1,5 +1,5 @@
 // Use environment variables for API URL
-export const API_URL = import.meta.env.VITE_API_URL || 'https://api.test.soss.site';
+export const API_URL = import.meta.env.VITE_API_URL ;
 
 export const getApiUrl = (path: string) => {
   return `${API_URL}${path.startsWith('/') ? path : `/${path}`}`;
